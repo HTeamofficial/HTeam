b9ab521ce434c7bb6910562863e0f3120f7938d9
HTeamofficial <chhc29058@gmail.com>
2025-01-10 16:44:36 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+let isMenuOpen = false;
+
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    isMenuOpen = !isMenuOpen;
+    menu.classList.toggle('hidden', !isMenuOpen);
+}
+
+function loadPage(page) {
+    alert('Chuyển đến trang: ' + page);
+}
+
+function login() {
+    alert('Chức năng đăng nhập đang được phát triển!');
+}
+
+function signup() {
+    alert('Chức năng đăng ký đang được phát triển!');
+}
+
+// Giả lập dữ liệu số dư
+document.getElementById('balance').innerText = 100; // Số dư HTC
+document.getElementById('frozen').innerText = 50; // Đóng băng
